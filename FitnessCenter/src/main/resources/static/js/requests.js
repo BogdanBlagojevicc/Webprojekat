6925b5af68fe5669f9d73c97185a2b9c2aaf6ab6
BogdanBlagojevicc <blagojevicbogdan1@gmail.com>
2021-06-08 11:59:50 +0200

    prikaz zahteva za registraciju trenera front

--- FitnessCenter/src/main/resources/static/js/requests.js
+++ FitnessCenter/src/main/resources/static/js/requests.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/api/users/trainersApplyForRegistration",
@@ -15,17 +16,40 @@ $(document).ready(function () {
                 row += "<td>" + requests.password + "</td>";
                 row += "<td>" + requests.birth + "</td>";
                 row += "<td>" + requests.phoneNumber + "</td>";
-                let btn = "<button class='btnSeeMore' data-id=" + requests.id + ">Accept</button>";
-                row += "<td>" + btn + "</td>";
-                btn = "<button class='btnDelete' data-id=" + requests.id + ">Decline</button>";
+                let btn = "<button class='btnAccept' data-id=" + requests.id + ">Accept</button>";
                 row += "<td>" + btn + "</td>";
                 row += "</tr>";
 
                 $('#requests').append(row);
             }
+
         },
         error: function (response) {
             console.log("ERROR:\n", response);
+        }
+    });
+});
+
+$(document).on("click", ".btnAccept", function () {
+
+    let trainerId = this.dataset.id;
+    let adminId = localStorage.getItem("id");
+    console.log("Trener", trainerId);
+    console.log("Admin", adminId);
+
+    $.ajax({
+        type: "PUT",
+        url: "http://localhost:8080/api/users/" + trainerId + "/" + adminId,
+        contentType: "application/json",
+        success: function (response) {
+            console.log("SUCCESS admin", response);
+
+            alert("Uspesna aktivacija trenera!");
+            window.location.href = "allTrainings.html";
+
+        },
+        error: function (response) {
+            alert("Greška pri promeni aktivnosti!");
         }
     });
 });
